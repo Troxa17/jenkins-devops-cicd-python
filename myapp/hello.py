7738a5b628592730c40a389cfad2537ad57de222
Troxa17 <118974468+Troxa17@users.noreply.github.com>
2023-09-07 20:11:40 +0300

    2.1.1

--- myapp/hello.py
+++ myapp/hello.py
@@ -3,11 +3,11 @@ import fire
 def hello(name="World"):
   return "Hello %s!" % name
 
-if __name__ == '__main__':
-  fire.Fire(hello)
-
 def tax(value):
-  return value*1.17
+  return int(value)*1.17
 
-if __value__ == '__main__'
+if __name__ == '__main__':
   fire.Fire(tax)
+
+
+
